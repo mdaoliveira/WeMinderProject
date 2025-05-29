cd547a3a50b4cefac2579a969093f1e632318d12
Felyppe Pardino <felyppepsilva@gmail.com>
2025-05-29 16:28:00 -0300

    Exibição de tarefas

--- frontend/weminderproject/src/App.jsx
+++ frontend/weminderproject/src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import Tasks from './components/Tasks/Tasks';
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
+      <Tasks/>
     </div>
   );
 }
---
+++ frontend/weminderproject/src/components/Tasks/Tasks.jsx
@@ -0,0 +1,16 @@
+function Tasks(){
+    return(
+        <div className="content">
+            <h1 className="data">29 de maio</h1>
+
+            <div className="task_box">
+                <div className="task_title"><h2>Título</h2></div>
+                <div className="task_desc"><p>Descrição</p></div>
+                <img src="/images/button.png" className="button_more"></img>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Tasks;
