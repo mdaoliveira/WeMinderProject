5283db0863adad989dbee4426cd9f66456e75623
leticia.garsztka <leticia.garsztka@gmail.com>
2025-05-29 15:59:03 -0300

    cadastroDeTarefas1

---
+++ frontend/weminderproject/src/components/CadastroDeTarefas/cadastroDeTarefas.jsx
@@ -0,0 +1,14 @@
+function CadastroDeTarefas(){
+    return(
+        <div class="cadastro-tarefas">
+            <form>
+                <label>
+                    <input type="radio">Tarefas Simples</input>
+                </label>
+                <label>
+                    <input type="radio">Tarefas Complexas</input>
+                </label>
+            </form>
+        </div>
+    );
+}
