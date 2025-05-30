a51b858719dc1e9778eeb8495811fbe261f7dfdb
Felyppe Pardino <felyppepsilva@gmail.com>
2025-05-30 20:40:59 -0300

    No cadastro de tarefas complexas, adição de subtarefas

--- frontend/weminderproject/src/components/CadastroDeTarefas/tarefasComplexas.jsx
+++ frontend/weminderproject/src/components/CadastroDeTarefas/tarefasComplexas.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 function TarefasComplexas(){
+
+    const [subtarefas, setSubtarefas] = useState([]);
+
+    const adicionarSubtarefa = () => {
+        setSubtarefas([...subtarefas, "subtarefa"]);
+    };
+
     return(
         <div className="cadastro-tarefa">
             <h2 className="titulo-div">Criar Tarefa Complexa</h2>
@@ -10,15 +19,31 @@ function TarefasComplexas(){
             <label className="content-cadastro">Descrição</label>
             <input type="text" id="descricao" name="texto"/>
 
-            <h3 className="cadastro-subtarefas">Subtarefas</h3>
+            <div className="subtarefas-header">
+                <h3 className="cadastro-subtarefas">Subtarefas</h3>
+                <label className="cadastro-subtarefas adicionar-subtarefa">
+                    <u onClick={adicionarSubtarefa}>Adicionar Subtarefa</u>
+                    <img src="/images/plus symbol.png" className="plus-symbol"></img>
+                </label>
+            </div>
+
             <label className="content-cadastro">Nome</label>
             <input type="text" id="nome" name="texto"/>
-
             <br/>
-
             <label className="content-cadastro">Descrição</label>
             <input type="text" id="descricao" name="texto"/>
            
+            {subtarefas.map((item) => (
+                <div>  
+                    <hr />
+                    <label className="content-cadastro">Nome</label>
+                    <input type="text" id="nome" name="texto"/>
+                    <br/>
+                    <label className="content-cadastro">Descrição</label>
+                    <input type="text" id="descricao" name="texto"/>
+                </div>
+            ))}
+
             <br/>
         </div>
     );
