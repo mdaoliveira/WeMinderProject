7103ab465074016c79b4eab52be3816c7ce48aed
leticia.garsztka <leticia.garsztka@gmail.com>
2025-05-30 10:52:08 -0300

    modal cadastro de tarefas

--- frontend/weminderproject/src/components/CadastroDeTarefas/cadastroDeTarefas.jsx
+++ frontend/weminderproject/src/components/CadastroDeTarefas/cadastroDeTarefas.jsx
@@ -1,17 +1,32 @@
+import React, {useState} from "react";
+import TarefasSimples from "./tarefasSimples";
+import TarefasComplexas from "./tarefasComplexas";
+
+
 function CadastroDeTarefas(){
+    const[tipoTarefa, setTipoTarefa] = useState(''); // mantém o item (radio) que foi selecionado
+
+    function handleTipoTarefa(e){
+        setTipoTarefa(e.target.value);
+    }
+
     return(
         <div className="cadastro-tarefas">
             <form>
                 <label>
-                    <input type="radio" name="tipoTarefa" />
+                    <input type="radio" name="tipoTarefa" value={"simples"} checked={tipoTarefa === 'simples'} onChange={handleTipoTarefa}/>
                     Tarefas Simples
                 </label>
                 <br></br>
                 <label>
-                    <input type="radio" name="tipoTarefa" />
-                    Tarefas Composta
+                    <input type="radio" name="tipoTarefa" value={"complexa"} checked={tipoTarefa === 'complexa'} onChange={handleTipoTarefa} />
+                    Tarefas Complexas
                 </label>
             </form>
+
+            {/*Colocando o componente conforme "solicitado" pelo usuário*/}
+            {tipoTarefa === 'simples' && <TarefasSimples/>}
+            {tipoTarefa === 'complexa' && <TarefasComplexas/>}
         </div>
     );
 }
