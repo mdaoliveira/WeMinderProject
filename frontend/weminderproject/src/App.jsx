e346db2663dfd2393ba818014b8d99d388fdd6d0
leticia.garsztka <leticia.garsztka@gmail.com>
2025-05-30 08:01:13 -0300

    funcao clicked

--- frontend/weminderproject/src/App.jsx
+++ frontend/weminderproject/src/App.jsx
@@ -5,11 +5,25 @@ import CadastroDeTarefas from './components/CadastroDeTarefas/cadastroDeTarefas'
 import Tasks from './components/Tasks/Tasks';
 
 function App() {
+  const[modalOpen, setModalIsOpen] = useState(false);
+  const[itemClicked, setItemClicked] = useState(null);
+
+  function clicked(item){
+    console.log("Clicou");
+    setModalIsOpen(true);
+    setItemClicked(item);
+  }
+
+  function closeModal(){
+    setModalIsOpen(false);
+    setItemClicked(null);
+  }
+
   return (
     <div className="App">
       <Sidebar/>
-      <CadastroDeTarefas/>
       <Tasks/>
+      <CadastroDeTarefas/>
     </div>
     
   );
